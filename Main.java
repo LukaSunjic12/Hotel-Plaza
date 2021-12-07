8eaaf0f48e95faa506a202147d97a2ad60a005a3
LukaSunjic12 <90600986+LukaSunjic12@users.noreply.github.com>
2021-12-07 13:46:53 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -13,10 +13,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        System.out.println("Hotel Plaza");
         Menu menu = new Menu();
-        menu.staffMenu();
-
-
+        menu.chooseMenu();
     }
 }
 
